2376e026c71def276d15e063b6b7c522fb70d220
PhySci <mushenokf@gmail.com>
2022-02-05 12:46:04 +0300

    Conflict

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 def main():
     df = pd.read("1.csv")
     df = df.drop("id", axis=1)
+    df.rename(columns={"age": "Age"}, inplace=True)
     return df
 
 if __name__ == "__main__":
